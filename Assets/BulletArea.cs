a4165975dbd23b18592a60b17da11cd52dba1eb7
Syed-BooAli-Naqvi <idofsyedbooalinaqvi@gmail.com>
2024-03-04 03:15:50 +0500

    Anims

--- Assets/BulletArea.cs
+++ Assets/BulletArea.cs
@@ -5,6 +5,47 @@ using UnityEngine;
 public class BulletArea : MonoBehaviour
 {
     public List<Bullets> bullets = new List<Bullets>();
+    public Animator animator;
+    public GameObject myCam;
+
+    private void Start()
+    {
+        StartCoroutine(StartShowing(5, 2));    
+    }
+
+    public IEnumerator StartShowing(int totalRounds, int empty)
+    {
+        myCam.SetActive(true);
+
+        yield return new WaitForSeconds(3);
+
+        for (int i = 0; i < bullets.Count; i++)
+        {
+            bullets[i].fullBullet.SetActive(false);
+            bullets[i].emptyBullet.SetActive(false);
+        }
+        for (int i = 0; i < totalRounds; i++)
+        {
+            bullets[i].fullBullet.SetActive(false);
+            bullets[i].emptyBullet.SetActive(true);
+        }
+        for (int i = 0; i < empty+1; i++)
+        {
+            bullets[i].fullBullet.SetActive(true);
+            bullets[i].emptyBullet.SetActive(false);
+        }
+        animator.SetTrigger("Open");
+        yield return new WaitForSeconds(4f);
+
+        animator.SetTrigger("Close");
+        yield return new WaitForSeconds(2f);
+
+
+        myCam.SetActive(false);
+        yield return new WaitForSeconds(2f);
+    }
+
+
 
     [System.Serializable]
     public struct Bullets
